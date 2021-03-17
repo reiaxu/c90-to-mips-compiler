5ed23511a88632e2ec64aa290f8d2b75dfea4f1e
reiaxu <59967035+reiaxu@users.noreply.github.com>
2021-03-17 14:38:15 +0000

    unary, partial postfix

---
+++ include/ast_postfixexpr.hpp
@@ -0,0 +1,54 @@
+#ifndef ast_postfixexpr_hpp
+#define ast_postfixexpr_hpp
+
+#include"ast_transalationalunit.hpp"
+
+#include<string>
+#include<map>
+#include<iostream>
+
+
+class PostfixExpr
+    : public TranslationalUnit{
+    
+    private:
+    TransUnitPtr postfixptr;
+    std::string* identif;
+    int op;
+
+public:
+    PostfixExpr(TransUnitPtr postfixptr_, int op_):postfixptr(postfixptr_),op(op_){}
+    PostfixExpr(TransUnitPtr postfixptr_, std::string* identif_, int op_):postfixptr(postfixptr_),identif(identif_),op(op_){}
+
+   
+    // PostfixExpr(std::string* t, TransUnitPtr p):type(t),ptr(p){} //case 0,2,6,7
+    // PostfixExpr(std::string* t, TransUnitPtr p, std::string * i):type(t),ptr(p), id(i){} //case 4,5
+    // PostfixExpr(std::string* t, TransUnitPtr p, TransUnitPtr o):type(t),ptr(p), op(o){} //case 1,3
+
+
+    ~PostfixExpr() override{
+        delete postfixptr;
+    }
+
+    virtual void PrettyPrint(std::ostream &dst) const override{
+        
+        if(op == 263){
+            postfixptr->PrettyPrint(dst);
+            dst<<"++";
+        } 
+        else if (op == 264) {
+            postfixptr->PrettyPrint(dst);
+            dst<<"--";
+        }
+        else if (op == 262) {
+            postfixptr->PrettyPrint(dst);
+            dst<<"* ";
+            dst<<(*identif);
+        }
+    }
+
+};
+
+
+
+#endif
---
+++ include/ast_unaryexpr.hpp
@@ -0,0 +1,49 @@
+#ifndef ast_unaryexpr_hpp
+#define ast_unaryexpr_hpp
+
+#include "ast_transalationalunit.hpp"
+#include <string>
+#include<map>
+#include<iostream>
+
+class UnaryExpr
+    :public TranslationalUnit{
+
+   private:
+    int prefix;
+    TransUnitPtr unaryexpr;
+
+public:
+    UnaryExpr(int u, TransUnitPtr p):prefix(u),unaryexpr(p){}
+
+
+    ~UnaryExpr() override{
+        
+        delete unaryexpr;
+    }
+
+    virtual void PrettyPrint(std::ostream &dst) const override{
+        
+        if(prefix<258){
+            dst<<char(prefix);
+            unaryexpr->PrettyPrint(dst);
+        }else{
+            // inc op
+            if (prefix==263) {
+                dst<<"++";
+                unaryexpr->PrettyPrint(dst);
+            }
+
+            // dec op
+            if (prefix==264) {
+                dst<<"--";
+                unaryexpr->PrettyPrint(dst);
+            }
+        }
+    }
+
+};
+
+
+
+#endif
