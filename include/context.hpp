825422b2f21a1c5237dc493eda494bc819e1668b
sdoif <sd2219@ic.ac.uk>
2021-03-20 21:00:58 +0000

    Temp fix for forward deleration issue

--- include/context.hpp
+++ include/context.hpp
@@ -1,13 +1,16 @@
 #ifndef context_hpp
 #define context_hpp
 
+#include"ast_transalationalunit.hpp"
+
 #include<string>
 #include<vector>
 #include <unordered_map>
-#include "ast.hpp"
+
+//typedef const TranslationalUnit *TransUnitPtr;
 
 struct type_pos{
-  TransUnitPtr type;
+  std::string type;
   int stack_offset;
 };
 
@@ -18,23 +21,27 @@ class Bindings{
     std::unordered_map< std::string,type_pos> var_bindings;
 
   public:
-    Bindings();
+    Bindings(std::string frame, int stackaddr):frame_id(frame),stack_frame(stackaddr){}
     ~Bindings(){
       //free memory with type_pos.type?
     };
 
-    void InsertBinding(const std::string _name,TransUnitPtr _type, int _stackpos){
+    void InsertBinding(const std::string _name,std::string _type, int _stackpos){
       type_pos varinfo;
       varinfo.type = _type;
       varinfo.stack_offset = _stackpos;
       var_bindings[_name] = varinfo;
     }
 
+    const int getOffset(const std::string _name){
+      return var_bindings[_name].stack_offset;
+    }
+
 };
 
 typedef struct var_param{
 
-  TransUnitPtr type;
+  std::string type;
   std::string name;
 
 }param;
