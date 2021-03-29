949740f576947ca1ac951b2fd2f5bb7c197221e3
reiaxu <59967035+reiaxu@users.noreply.github.com>
2021-03-29 14:19:20 +0100

    output passed and failed list

--- run_test_all.sh
+++ run_test_all.sh
@@ -12,6 +12,9 @@ mkdir working
 PASSED=0
 CHECKED=0
 
+allPASSED=()
+allFAILED=()
+
 for CATEGORY in compiler_tests/* ; do
     SECTION=$(basename $CATEGORY)
     mkdir -p working/$SECTION
@@ -27,18 +30,21 @@ for CATEGORY in compiler_tests/* ; do
         bin/c_compiler -S compiler_tests/$SECTION/$NAME.c -o working/${SECTION}/${NAME}.s
         if [[ $? -ne 0 ]]; then
             printf "\e[1;31mError\e[0m : Couldn't output to .s\n"
+            allFAILED+=( $NAME )
             continue
         fi
 
         mips-linux-gnu-gcc -mfp32 -o working/${SECTION}/${NAME}.o -c working/${SECTION}/${NAME}.s
         if [[ $? -ne 0 ]]; then
             printf "\e[1;31mError\e[0m : Couldn't compile driver program using GCC.\n"
+            allFAILED+=( $NAME )
             continue
         fi
 
         mips-linux-gnu-gcc -mfp32 -static -o working/${SECTION}/${NAME} working/${SECTION}/${NAME}.o compiler_tests/${SECTION}/${NAME}_driver.c
         if [[ $? -ne 0 ]]; then
             printf "\e[1;31mError\e[0m : Linker returned error message.\n"
+            allFAILED+=( $NAME )
             continue
         fi
 
@@ -46,14 +52,19 @@ for CATEGORY in compiler_tests/* ; do
         RESULT=$?
         if [[ "$RESULT" -ne 0 ]]; then
             printf "\e[1;31mError\e[0m : Testcase returned $RESULT, but expected 0.\n"
+            allFAILED+=( $NAME )
             continue
         fi
 
         printf "\e[1;32mPass\e[0m\n"
         PASSED=$(( PASSED+1 ))
+        allPASSED+=( $NAME )
     done
 done
 
 echo "########################################"
 echo " Passed ${PASSED} out of ${CHECKED}"
 echo ""
+echo "PASSED: ${allPASSED[@]}	 "
+echo "########################################"
+echo "FAILED: ${allFAILED[@]} "
